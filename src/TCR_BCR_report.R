269aca1ed7d95a779075856704ba0f0f3ebe884c
MDMorgan <michael.morgan@cruk.cam.ac.uk>
2020-05-15 14:08:50 +0100

    add try catch to deal with QC-fail issues

--- src/TCR_BCR_report.R
+++ src/TCR_BCR_report.R
@@ -13,23 +13,6 @@ library(optparse)
 # rather than pull in all of edgeR we only need this one function
 # it operates over a matrix and returns a vector
 
-gini <- function(x)
-#    Gini diversity index for columns of a numeric matrix
-#    Gordon Smyth
-#    Created 5 Feb 2016. Last revised 17 April 2017.
-{
-	x <- as.matrix(x)
-	d <- dim(x)
-	for (j in 1:d[2]) x[,j] <- sort.int(x[,j],na.last=TRUE)
-	i <- 1:d[1]
-	m <- 0.75*d[1]
-	S1 <- colSums((i-m)*x , na.rm=TRUE)
-	S2 <- colSums(x, na.rm=TRUE)
-	(2*(S1/S2+m)-d[1]-1)/d[1]
-}
-
-
-
 parser <- OptionParser() 
 parser <- add_option(parser, c("-l", "--logs"), type="character",
    	  	     help="Path to csv for summary (optional)")
