7c21588a9c9f1968b11ca7c34cdabbb28fd541d4
MikeDMorgan <michael.morgan@cruk.cam.ac.uk>
2021-01-12 11:52:32 +0000

    update snake make pipeline

--- src/identify_doublets.R
+++ src/identify_doublets.R
@@ -10,14 +10,17 @@ parser <- add_option(parser, c("-d", "--doubletdata"), type="character",
 		     help="Comma separated list of doublet data files")
 parser <- add_option(parser, c("-o", "--out"), type="character", 
 		     help="Path to .csv")
+parser <- add_option(parser, c("-s", "--SCE"), type="character", 
+		     help="Path to SCE object")
 parser <- add_option(parser, c("-p", "--plot"), type="character", 
 		     help="Path to QC plots")
 opt <- parse_args(parser)
 
-
 # ---- Load Data ----
 library(ggplot2)
 library(cowplot)
+library(scran)
+library(BiocSingular)
 theme_set(theme_cowplot())
 
 dd.files <- unlist(strsplit(opt$doubletdata,","))
@@ -65,12 +68,13 @@ cluster.scores$fracVireoDoublets = sapply(1:nrow(cluster.scores), function(RW){
 message("Determining doublets")
 # Define doublets based on fraction and median doublet score
 # Will have to see whether it is sensible to identify outlier per sample rather than across all samples
-cluster.scores$ScoreOutlier <- scater::isOutlier(cluster.scores$Medscore,nmad=1.5,type="higher",log=FALSE)
+cluster.scores$ScoreOutlier <- scater::isOutlier(cluster.scores$Medscore,nmad=2.5,type="higher",log=FALSE)
 cluster.scores$ScoreIsDoublet <- cluster.scores$ScoreOutlier & cluster.scores$fracCells <= 0.07
 
 
 # Define doublets based on fraction of cells within a cluster that are genotype doublets
-cluster.scores$VireoOutlier <- cluster.scores$fracVireoDoublets > 0.3
+cluster.scores$VireoOutlier <- scater::isOutlier(cluster.scores$fracVireo,nmad=2.5,type="higher",log=FALSE,
+						 batch=cluster.scores$Sample)
 cluster.scores$VireoIsDoublet <- cluster.scores$VireoOutlier & cluster.scores$fracCells <= 0.07
 
 
@@ -118,9 +122,8 @@ names(doublet_plots) <- smps
 p_umaps <- plot_grid(plotlist=doublet_plots)
 
 
-#### All of the below is just to label how each doublet was identified to make some checks later, might rm all of the below eventually
+# Finalize per-sample doublet definition
 
-message("Preparing output")
 # Cells identified by vireo
 vir.dblts <- ddata.df[ddata.df$Donor=="doublet","Barcode"]
 
@@ -142,9 +145,91 @@ vir.dblts <- vir.dblts[!vir.dblts %in% mult.dblts]
 scr.dblts <- scr.dblts[!scr.dblts %in% mult.dblts]
 virCluster.dblts <- virCluster.dblts[!virCluster.dblts %in% mult.dblts]
 
-# Combine everything for output
-all.dblts <- c(vir.dblts,scr.dblts,virCluster.dblts,mult.dblts)
+per.smp.dblts <- c(vir.dblts,scr.dblts,virCluster.dblts,mult.dblts)
 
+# ---- Cluster across whole dataset ----
+message("Clustering whole dataset")
+library(BiocNeighbors)
+sce <- readRDS(opt$SCE)
+dec <- modelGeneVar(sce)
+hvgs <- getTopHVGs(dec)
+set.seed(42)
+
+# PCA
+sce <- denoisePCA(sce, technical=dec,
+		       subset.row=hvgs,
+		       BSPARAM=BiocSingular::IrlbaParam())
+# Graph with low k
+igr <- buildSNNGraph(sce,
+		     use.dimred="PCA",
+		     k=10,
+		     BNPARAM=AnnoyParam()) 
+
+# Clustering on graph
+cl <- igraph::cluster_louvain(igr)
+sce$Cluster <- paste0("C",cl$membership)
+
+# Sub Clustering
+set.seed(42)
+cluster.list <- quickSubCluster(sce, groups=sce$Cluster,
+				  min.ncells=200,
+    prepFUN=function(x) { 
+        dec <- modelGeneVar(x)
+        input <- denoisePCA(x, technical=dec,
+            subset.row=getTopHVGs(dec),
+            BSPARAM=BiocSingular::IrlbaParam())
+    },
+    clusterFUN=function(x) { 
+        g <- buildSNNGraph(x, use.dimred="PCA", k=20,
+			   BNPARAM=AnnoyParam())
+        igraph::cluster_louvain(g)$membership
+    }
+)
+
+# Extract barcode to cluster assignment
+getSubClusters <- function(sce.obj){
+    bcs <- colnames(sce.obj)
+    clst <- as.character(colData(sce.obj)$subcluster)
+    out <- data.frame("Barcode"=bcs,
+		      "Cluster"=clst)
+    return(out)
+}
+
+# Compute a final UMAP of entire datasets
+pcs <- reducedDim(sce)
+library(umap)
+ump <- umap(pcs, random_state=42)
+ump <- data.frame("UMAP1"=ump$layout[,1],
+		  "UMAP2"=ump$layout[,2],
+		  "Barcode"=rownames(pcs))
+
+
+clust.full <- lapply(cluster.list,getSubClusters)
+cluster.ids <- do.call(rbind,clust.full)
+colnames(cluster.ids)[2] <- "FullDataCluster"
+
+# Add cluster assignment to ddatadf
+ddata.df <- dplyr::left_join(ddata.df,cluster.ids)
+ddata.df$PerSampleDoublet <- ddata.df$Barcode %in% per.smp.dblts
+
+library(dplyr)
+smry <- group_by(ddata.df, FullDataCluster, PerSampleDoublet) %>%
+    summarize(NType=n()) %>%
+    mutate(Prop=NType/sum(NType))
+
+doubDef <- smry$Prop[smry$PerSampleDoublet] > 0.6
+dblClusters <- smry$FullDataCluster[smry$PerSampleDoublet][doubDef]
+
+#Define cells that are defined by guilty-by-association
+ga.dblts <- ddata.df$Barcode[ddata.df$FullDataCluster %in% dblClusters]
+ga.dblts <- ga.dblts[!ga.dblts %in% per.smp.dblts]
+
+# ---- Save ----
+message("Preparing output")
+
+# Combine everything for output
+
+all.dblts <- c(per.smp.dblts,ga.dblts)
 ddata.df.dblts <- ddata.df[ddata.df$Barcode %in% all.dblts,]
 
 out <- data.frame("Barcode"=ddata.df.dblts$Barcode)
@@ -152,12 +237,39 @@ out$DoubletType <- plyr::mapvalues(all.dblts,out$Barcode,
 				   c(rep("Vireo",length(vir.dblts)),
 				     rep("Score",length(scr.dblts)),
 				     rep("VirCluster",length(virCluster.dblts)),
-				     rep("Multiple",length(mult.dblts))))
-
-# ---- Save ----
+				     rep("Multiple",length(mult.dblts)),
+				     rep("GBA",length(ga.dblts))))
 message("Saving Output")
 write.csv(out,opt$out)
 name.scores <- paste0(opt$pl,"/DoubletScores.pdf")
 name.umaps <- paste0(opt$pl,"/DoubletUMAPs.pdf")
+
 ggsave(p_scores,file=name.scores)
-ggsave(p_umaps,file=name.umaps)
+
+# Save UMAPS on multiple pages of a single PDF
+ln <- length(doublet_plots)
+brks <- floor(ln/4)
+brks <- 1:brks * 4
+if (brks[length(brks)]!=ln) {
+    brks <- c(brks,ln)
+}
+pdf(name.umaps, onefile=TRUE, width=10,height=10)
+for (i in seq_along(brks)) {
+    bgn <- 4 * (i-1) +1
+    plot(plot_grid(plotlist=doublet_plots[bgn:brks[i]]))
+}
+# Add in Overall UMAP
+ump <- left_join(ump,out)
+ump$IsDoublet <- ump$Barcode %in% all.dblts
+p <- ggplot(ump, aes(x=UMAP1, y=UMAP2)) +
+	   geom_point(color="grey80",size=0.1) +
+	   geom_point(data=ump[ump$IsDoublet,], aes(color=DoubletType),size=.1) +
+	   theme(legend.position="none", panel.background = element_rect(colour = "black", linetype = 1, size = 0.5),
+		 axis.line = element_blank(),
+		 axis.ticks = element_blank(),
+		 axis.text = element_blank(),
+		 axis.title = element_blank(),) +
+	   ggtitle(paste0("Full Dataset of ", nrow(ump), " with ", sum(ump$IsDoublet)," doublets")) +
+	   facet_wrap(~DoubletType)
+print(p)
+dev.off()
--- src/train_mofa.py
+++ src/train_mofa.py
@@ -12,19 +12,40 @@ import mofapy2
 from mofapy2.run.entry_point import entry_point
 import pandas as pd
 import numpy as np
+from scipy import io as scio
 
-parser = argparse.ArgumentParser(description="Extract reads from ADT library given an index")
+parser = argparse.ArgumentParser(description="Run MOFA on input multi-omics matrices")
 
 parser.add_argument("--counts-list", dest="counts_files", type=str,
-                    help="A comma-separated list of paths to ADT counts matrices (txt format)")
+                    help="A comma-separated list of paths to input matrices (txt format)")
+
+parser.add_argument("--omic", dest="omic", type=str,
+                    choices=["ADT", "GEX", "joint"],
+                    help="The type of input omic, ADT, GEX, or joint for both")
 
 parser.add_argument("--donor-list", dest="donor_files", type=str,
-                    help="A comma-separated list of paths to donor ID information")
+                    help="A comma-separated list of paths to donor ID information, "
+                    "must be of the same length as the number of files")
+
+parser.add_argument("--gex-rownames", dest="gex_rownames", type=str,
+                    default=None, help="A path to a file containing the GEX rownames")
+
+parser.add_argument("--adt-rownames", dest="adt_rownames", type=str,
+                    default=None, help="A path to a file containing the ADT rownames")
+
+parser.add_argument("--gex-colnames", dest="gex_colnames", type=str,
+                    default=None, help="A path to a file containing the GEX colnames")
+
+parser.add_argument("--adt-colnames", dest="adt_colnames", type=str,
+                    default=None, help="A path to a file containing the ADT colnames")
 
 parser.add_argument("--format", dest="data_format", type=str,
                     choices=["wide", "long"], default="wide",
                     help="Input data format, either multiple data matrices in wide format, or a "
                     "single long format data frame.")
+
+parser.add_argument("--group-column", dest="group_col", type=str,
+                    help="Column from donors file that will be used for MOFA groups")
 
 parser.add_argument("--factors", dest="factors", type=int,
                     help="The number of factors to learn")
@@ -71,11 +92,22 @@ mofa_ent = entry_point()
 file_list = args.counts_files.split(",")
 
 # read in the donor data frames as dict for reference
+# must be the same number of files unless running joint mode
+logging.info("Readining in donor information: {}".format(args.donor_files))
 donor_list = args.donor_files.split(",")
-donor_names = [FX.split("/")[-2] for FX in donor_list]
 
-donor_dict = dict(zip(donor_names,
-                      [pd.read_table(D, sep="\t", header=0, index_col=None) for D in donor_list]))
+if len(donor_list) != len(file_list):
+    if args.omic == 'joint' and len(donor_list) == 1:
+        donor_names = ["Gene", "ADT"]
+        donor_df_list = [pd.read_table(D, sep="\t", header=0, index_col='CellID') for D in donor_list]
+        donor_dict = dict(zip(donor_names,
+                              donor_df_list * len(donor_names)))
+    else:
+        raise("The number of donor files is not the same as the input files")
+else:
+    donor_names = [FX.split("/")[-2] for FX in donor_list]
+    donor_dict = dict(zip(donor_names,
+                          [pd.read_table(D, sep="\t", header=0, index_col=None) for D in donor_list]))
 
 # keep only the singlets
 keep_dict = {}
@@ -85,19 +117,99 @@ keep_dict = {}
 
 matrix_list = []
 
+# read in dimension names as pandas series
+# need to change ".1" to "-1" artefact from R
+logging.info("Reading in data dimension names")
+dim_dict = {}
+logging.info("GEX features: {}".format(args.gex_rownames))
+gex_rows = pd.read_table(args.gex_rownames, sep="\t", index_col=None, header=None)
+logging.info("GEX cell IDs: {}".format(args.gex_colnames))
+gex_cols = pd.read_table(args.gex_colnames, sep="\t", index_col=None, header=None)
+logging.info("ADT features: {}".format(args.adt_rownames))
+adt_rows = pd.read_table(args.adt_rownames, sep="\t", index_col=None, header=None)
+logging.info("ADT cell IDs: {}".format(args.adt_colnames))
+adt_cols = pd.read_table(args.adt_colnames, sep="\t", index_col=None, header=None)
+
+dim_dict['ADT'] = {'rows': adt_rows.iloc[:, 0].values,
+                   'cols': [ax.replace('.1', '-1') for ax in adt_cols.iloc[:, 0].values]}
+dim_dict['GEX'] = {'rows': gex_rows.iloc[:, 0].values,
+                   'cols': [gx.replace('.1', '-1') for gx in gex_cols.iloc[:, 0].values]}
+
+logging.info("Populating dimension dictionary, keys: {}".format(dim_dict.keys()))
+
 for x in range(len(file_list)):
     x_file = file_list[x]
     # extract the sample name from the matrix file
     # assumes the first column contains the relevant feature IDs
     if args.data_format == "wide":
-        x_matrix = pd.read_table(x_file, delimiter="\t", compression="gzip",
-                                 header=0, index_col=None)
+        if re.search("gz$", x_file):
+            x_matrix = pd.read_table(x_file, delimiter="\t", compression="gzip",
+                                     header=0, index_col=None)
+        elif re.search("mtx$", x_file):
+            logging.info("Reading matrix file: {}".format(x_file))
+            x_matrix = scio.mmread(x_file)
+            if args.omic == "joint":
+                if re.search("ADT", x_file):
+                    logging.info("Adding ADT dimension names: {}".format(x_matrix.shape))
+                    x_matrix = pd.DataFrame.sparse.from_spmatrix(x_matrix,
+                                                                 index=dim_dict['ADT']['rows'],
+                                                                 columns=dim_dict['ADT']['cols'])
+                    x_matrix["ADT"] = dim_dict['ADT']['rows']
+
+                elif re.search("GEX", x_file):
+                    logging.info("Adding GEX dimension names: {}".format(x_matrix.shape))
+                    x_matrix = pd.DataFrame.sparse.from_spmatrix(x_matrix,
+                                                                 index=dim_dict['GEX']['rows'],
+                                                                 columns=dim_dict['GEX']['cols'])
+                    x_matrix["Gene"] = dim_dict['GEX']['rows']
+
+            elif args.omic == "GEX":
+                logging.info("Adding GEX dimension names: {}".format(x_matrix.shape))
+                x_matrix = pd.DataFrame.sparse.from_spmatrix(x_matrix,
+                                                             index=dim_dict['GEX']['rows'],
+                                                             columns=dim_dict['GEX']['cols'])
+                x_matrix["Gene"] = dim_dict['GEX']['rows']
+
+            elif args.omic == "ADT":
+                logging.info("Adding ADT dimension names: {}".format(x_matrix.shape))
+                x_matrix = pd.DataFrame.sparse.from_spmatrix(x_matrix,
+                                                             index=dim_dict['ADT']['rows'],
+                                                             columns=dim_dict['ADT']['cols'])
+                x_matrix["ADT"] = dim_dict['ADT']['rows']
+        else:
+            x_matrix = pd.read_table(x_file, delimiter="\t",
+                                     header=0, index_col=None)
 
         logging.info("Melting matrices into long data frames")
-        x_melt = x_matrix.melt(id_vars="ADT")
+        # melt based on omic type
+        if args.omic == "ADT":
+            melt_id = "ADT"
+        elif args.omic == "GEX":
+            melt_id = "Gene"
+        elif args.omic == "joint":
+            # infer from filename
+            if re.search("ADT", x_file):
+                melt_id = "ADT"
+            elif re.search("GEX", x_file):
+                melt_id = "Gene"
+
+        # need to test if key is in the columns, if not then use the index
+        try:
+            x_melt = x_matrix.melt(id_vars=melt_id)
+        except KeyError:
+            x_melt = x_matrix.melt()
+        print(x_melt.head())
+
         x_melt.columns = ["feature", "sample", "value"]
-        x_melt.loc[:, "view"] = "ADT"
-        x_melt.loc[:, "group"] = donor_names[x]
+        x_melt.loc[:, "view"] = melt_id
+        if args.omic == "joint":
+            donor_df = donor_dict[melt_id]
+            # remove cell Ids from the donor list that don't appear in the ADT data
+            donor_df = donor_df.loc[donor_df.index.isin(x_melt["sample"].unique()), :]
+            x_melt = x_melt.merge(donor_df.loc[:, [args.group_col]], how='left', left_on='sample', right_index=True)
+            x_melt.columns = ["feature", "sample", "value", "view", "group"]
+        else:
+            x_melt.loc[:, "group"] = donor_names[x]
         print(x_melt.head())
 
     elif args.data_format == "long":
@@ -112,6 +224,11 @@ for x in range(len(file_list)):
     # make one big data frame
     dt_frame = pd.concat(matrix_list)
 
+    # check there are no NaNs in the groups - this has caused a massive headache.
+    print(dt_frame.loc[dt_frame["group"].isnull(), "sample"].str.extract(pat="(BGCV[0-9]+)_\S+"))
+    dt_frame.loc[dt_frame["group"].isnull(), "group"] = dt_frame.loc[dt_frame["group"].isnull(), "sample"].str.extract(pat="(BGCV[0-9]+)_\S+").values
+    print(dt_frame[dt_frame["group"].isnull()].head())
+
 if args.ignore_groups:
     logging.info("Ignore groups label from data - running in single group mode")
     # set the group value to all 0
@@ -125,7 +242,9 @@ mofa_ent.set_data_options(
 
 # add the data to the model
 logging.info("Setting up MOFA data options")
-mofa_ent.set_data_df(dt_frame, likelihoods = ["gaussian"])
+print(dt_frame.head())
+mofa_ent.set_data_df(dt_frame)
+#mofa_ent.set_data_df(dt_frame, likelihoods = ["gaussian" for x in range(len(file_list))])
 
 logging.info("Setting up MOFA model options")
 mofa_ent.set_model_options(
@@ -137,12 +256,11 @@ mofa_ent.set_model_options(
 
 logging.info("Setting up training options")
 mofa_ent.set_train_options(
-    iter = 1000, 
-    convergence_mode = args.runmode, 
-    #drop_factor_threshold = -1,
+    iter = 1000,
+    convergence_mode = args.runmode,
     startELBO = 1, 
     freqELBO = 1, 
-    dropR2 = 0.0000001, 
+    dropR2 = None, 
     gpu_mode = False,
     verbose = False,
     seed = 42
